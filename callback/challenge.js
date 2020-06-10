1693c66e6c2d651bf5b82505206fcb9204f73368
sebasrodri237 <sebasplay2@hotmail.com>
2020-06-10 01:09:55 -0500

    clase 6 - Múltiples Peticiones a un API con Callbacks

--- callback/challenge.js
+++ callback/challenge.js
@@ -1,11 +1,12 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 //Incluimos el modulo en nuestro proyecto. requiero(nombre_modulo).propiedad
 //para hacer instancias(llamados) a una API desde JS
+let API = 'https://rickandmortyapi.com/api/character/';//La API a la que haremos peticiones
 
 function fetchData(url_api, callback){
 
     let xhttp = new XMLHttpRequest();//Generamos referencia al objeto que necesito(en este caso xmlht...)
-    xhttp.open('GET', url_api, true)//Hacer llamados a una url.
+    xhttp.open('GET', url_api, true);//Hacer llamados a una url.
     //Obtener info, url de la data, activar el asincronismo(por defecto es true)
     xhttp.onreadystatechange = function(e){//Escuchar sobre la conexion y llamar
         //una funcion cuando ha cambiado el estado. //Mas info https://www.w3schools.com/xml/ajax_xmlhttprequest_response.asp
@@ -25,3 +26,32 @@ function fetchData(url_api, callback){
     xhttp.send();//Enviar la solicitud
 }
 //Crear una funcion que permite traer info desde la API
+
+fetchData(API, function(error1, data1){
+
+    if(error1){
+
+        return console.error(error1);
+    }
+    fetchData(API + data1.results[0].id, function(error2,data2){
+
+        if(error2){
+
+            return console.error(error2);
+        }
+        fetchData(data2.origin.url, function(error3, data3){
+            
+            if(error3){
+
+                return console.error(error3);
+            }
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+            //Hay que hacer 3 peticiones a la API porque no deja traer
+            //todos los valores en una sola peticion
+            //Encadenar mas peticiones seria
+            // el callback hell. Una mala practica.
+        })
+    })
+})
